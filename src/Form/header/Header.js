47affd8a0f62a9b9cdda1f00e513c036b4afdb17
rayasalem <rayasalem483@gmail.com>
2023-09-07 10:27:08 +0300

    add readmefile

--- src/Form/header/Header.js
+++ src/Form/header/Header.js
@@ -2,7 +2,7 @@ import './Header.css';
 function  Header (){
     return(
         <div className="header"> 
-        <h1>Calculate!</h1>
+        <h1 className='text'>Calculate!</h1>
         </div>
     );
 }
